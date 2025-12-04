b0039f82770d56554549a3ae6af0da22371b6613
agnibrata98 <agnibratashee@gmail.com>
2025-12-04 17:33:14 +0530

    solving some issues third

--- app/config/emailConfig.ts
+++ app/config/emailConfig.ts
@@ -4,9 +4,10 @@ import nodemailer from "nodemailer";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: Number(process.env.EMAIL_PORT) || 465,
-  secure: true, // true for 465, false for other ports
+  // host: process.env.EMAIL_HOST || "smtp.gmail.com",
+  // port: Number(process.env.EMAIL_PORT) || 465,
+  // secure: true, // true for 465, false for other ports
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER, // Admin Gmail ID
     pass: process.env.EMAIL_PASS, // Admin Gmail Password or App Password
@@ -14,10 +15,10 @@ export const transporter = nodemailer.createTransport({
 });
 
 // Optional: verify connection configuration
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("❌ Email transporter error:", error);
-  } else {
-    console.log("✅ Email transporter ready");
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.error("❌ Email transporter error:", error);
+//   } else {
+//     console.log("✅ Email transporter ready");
+//   }
+// });
--- dist/app/config/emailConfig.js
+++ dist/app/config/emailConfig.js
@@ -8,20 +8,20 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const nodemailer_1 = __importDefault(require("nodemailer"));
 dotenv_1.default.config();
 exports.transporter = nodemailer_1.default.createTransport({
-    host: process.env.EMAIL_HOST || "smtp.gmail.com",
-    port: Number(process.env.EMAIL_PORT) || 465,
-    secure: true, // true for 465, false for other ports
+    // host: process.env.EMAIL_HOST || "smtp.gmail.com",
+    // port: Number(process.env.EMAIL_PORT) || 465,
+    // secure: true, // true for 465, false for other ports
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER, // Admin Gmail ID
         pass: process.env.EMAIL_PASS, // Admin Gmail Password or App Password
     },
 });
 // Optional: verify connection configuration
-exports.transporter.verify((error, success) => {
-    if (error) {
-        console.error("❌ Email transporter error:", error);
-    }
-    else {
-        console.log("✅ Email transporter ready");
-    }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.error("❌ Email transporter error:", error);
+//   } else {
+//     console.log("✅ Email transporter ready");
+//   }
+// });
